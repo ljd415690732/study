f0ae9a86efefd8bd615e2e4828de95e6a8be4388
liujidong <415690732@qq.com>
2020-04-09 16:34:06 +0800

    测试合并

--- src/main/resources/com/jido/study/HelloWorld.java
+++ src/main/resources/com/jido/study/HelloWorld.java
@@ -3,6 +3,9 @@ package com.jido.study;
 public class HelloWorld {
 
     public static void main(String[] args){
+        System.out.print("test1222");
         System.out.print("test11111");
+
+        System.out.print("test333");
     }
 }
